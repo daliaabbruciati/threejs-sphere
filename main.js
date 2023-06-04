ec78390bab32c3b0ba92c8dc36881534b2dac902
Dalia Abbruciati <daliaabbruciati@gmail.com>
2023-06-04 23:59:27 +0200

    ThreeJS part

---
+++ main.js
@@ -0,0 +1,65 @@
+import *  as THREE from 'three'
+import './style.css'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+
+// Scene
+const scene = new THREE.Scene()
+
+// Create the sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64)
+const material = new THREE.MeshStandardMaterial({
+    color: '#00ff83',
+})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+}
+
+// Lights
+const light = new THREE.PointLight(0xffffff,1,100)
+light.position.set(0,10,10)
+scene.add(light)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(45,sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 20
+scene.add(camera)
+
+// Render the sphere on the screen with canvas
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(2)
+renderer.render(scene,camera)
+
+// Controls:
+// - OrbitControls: make the sphere rotating when pointing and dragging the scene
+const controls = new OrbitControls(camera,canvas)
+controls.enableDamping = true
+controls.enablePan = false
+controls.enableZoom = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 5
+
+// Resize
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+    renderer.setSize(sizes.width, sizes.height)
+})
+
+const loop = () => {
+    // to continue moving the sphere when i release the drag
+    controls.update()
+    renderer.render(scene,camera)
+    window.requestAnimationFrame(loop)
+}
+loop()
